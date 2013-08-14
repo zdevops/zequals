3652a81a989a43968f6993360dd2d9ab9b6181ce
Henri Kuiper <henri@Henris-MacBook-Pro.local>
2013-08-14 21:59:30 +0200

    with setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name = "zequals",
+    packages = ["zequals"],
+    version = "0.0.3",
+	description = "Ruthless Rounding at it's Finest :)",
+    author = "Henri Kuiper",
+    author_email = "henrikuiper@zdevops.com",
+	url = "http://www.youtube.com/watch?v=aOJOfh2_4PE",
+    download_url = "",
+    keywords = ["numberphile", "Rob Eastaway", "zequalize"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        ],
+)
